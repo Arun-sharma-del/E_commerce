b9bf15c6f1f5f88b236dad008b0521bfb4a30ff4
Arun-sharma-del <arun162000sharma@gmail.com>
2025-07-28 19:22:49 +0530

    Update models.py

--- store/models.py
+++ store/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.urls import reverse
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
+from .product import Product
+
 class Category(models.Model):
     name = models.CharField(max_length=200)
     slug = models.SlugField(unique=True)
@@ -13,6 +15,12 @@ class Category(models.Model):
         related_name='subcategories'
 
     )
+
+
+    class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+    is_paid = models.BooleanField(default=False)
 
     def __str__(self):
         return self.name
