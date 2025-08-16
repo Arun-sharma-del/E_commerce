5b6f858d66d3204402295d4e6ba25a4de67eb5c9
Arun-sharma-del <arun162000sharma@gmail.com>
2025-08-16 17:38:09 +0530

    pro

--- store/models.py
+++ store/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.urls import reverse
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
-from .product import Product
+
 
 class Category(models.Model):
     name = models.CharField(max_length=200)
